3b667c61351c016d48c09e667fdd9bdeeddf2729
Myungin Lee <benedic.lee@gmail.com>
2025-02-18 13:20:39 -0500

    6. Lerp 2 lerp update.

--- Assets/Scenes/Audio/AudioLerp.cs
+++ Assets/Scenes/Audio/AudioLerp.cs
@@ -1,12 +1,13 @@
 // UMD IMDM290 
 // Instructor: Myungin Lee
-// All the same Lerp but using microphone
+// All the same Lerp but using audio
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class MicReactive : MonoBehaviour
+public class AudioLerp : MonoBehaviour
 {
     GameObject[] spheres;
     static int numSphere = 200; 
@@ -14,7 +15,9 @@ public class MicReactive : MonoBehaviour
     Vector3[] initPos;
     Vector3[] startPosition, endPosition;
     float lerpFraction; // Lerp point between 0~1
-    float t;
+    float t , phase;
+    GameObject mother;
+    float direction = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +26,9 @@ public class MicReactive : MonoBehaviour
         spheres = new GameObject[numSphere];
         initPos = new Vector3[numSphere]; // Start positions
         startPosition = new Vector3[numSphere]; 
-        endPosition = new Vector3[numSphere]; 
-        
+        endPosition = new Vector3[numSphere];
+        mother = GameObject.Find("leafBody");
+        phase = 0f;
         // Define target positions. Start = random, End = heart 
         for (int i =0; i < numSphere; i++){
             // Random start positions
@@ -39,12 +43,14 @@ public class MicReactive : MonoBehaviour
         for (int i =0; i < numSphere; i++){
             // Draw primitive elements:
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/GameObject.CreatePrimitive.html
-            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+            //spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            spheres[i] = Instantiate(mother);
 
             // Position
             initPos[i] = startPosition[i];
             spheres[i].transform.position = initPos[i];
-
+            spheres[i].transform.localRotation = Quaternion.EulerAngles(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f));
+            spheres[i].transform.localScale = new Vector3(Random.Range(0.2f, 0.3f), Random.Range(0.2f, 0.3f), Random.Range(0.2f, 0.3f));
             // Color
             // Get the renderer of the spheres and assign colors.
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
@@ -62,22 +68,28 @@ public class MicReactive : MonoBehaviour
         // Measure Time 
         // Time.deltaTime = The interval in seconds from the last frame to the current one
         // but what if time flows according to the music's amplitude?
-        time += Time.deltaTime * MicInput.Amplitude; 
+        time += Time.deltaTime * AudioSpectrum.audioAmp;
+        // time and phase is different.
+        phase += Time.deltaTime * direction;
+
         // what to update over time?
         for (int i =0; i < numSphere; i++){
             // Lerp : Linearly interpolates between two points.
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
             // Vector3.Lerp(startPosition, endPosition, lerpFraction)
-            
+
             // lerpFraction variable defines the point between startPosition and endPosition (0~1)
-            lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f;
+            float start = 0f;
+            float end = 1f;
+
+            lerpFraction = Mathf.Lerp(start, end, phase/10f);
 
             // Lerp logic. Update position       
             t = i* 2 * Mathf.PI / numSphere;
             spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
-            float scale = 1f + MicInput.Amplitude;
-            spheres[i].transform.localScale = new Vector3(scale, 1f, 1f);
-            spheres[i].transform.Rotate(MicInput.Amplitude, 1f, 1f);
+            float scale = (1f + AudioSpectrum.audioAmp) * 0.2f;
+            //spheres[i].transform.localScale = new Vector3(scale, 0.2f, 0.2f);
+            spheres[i].transform.Rotate(AudioSpectrum.audioAmp, 1f, 1f);
             
             // Color Update over time
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
@@ -85,5 +97,34 @@ public class MicReactive : MonoBehaviour
             Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Cos(time)), Mathf.Cos(AudioSpectrum.audioAmp / 10f), 2f + Mathf.Cos(time)); // Full saturation and brightness
             sphereRenderer.material.color = color;
         }
+
+
+
+        // Change trigger happens when..
+        if (phase >= 10.0f | phase <= 0f)
+        {
+            Debug.Log("switch");
+
+            // change phase sequence direction 
+            direction = direction * (-1f);
+
+            // update the end position 
+            if (phase <= 0f)
+            {
+                
+                for (int i = 0; i < numSphere; i++)
+                {
+                    t = i * 2 * Mathf.PI / numSphere;
+                    endPosition[i] = new Vector3(
+                            5f * Mathf.Sqrt(2f) * Mathf.Sin(t) * Mathf.Sin(t) * Mathf.Sin(t),
+                            5f * (-Mathf.Cos(t) * Mathf.Cos(t) * Mathf.Cos(t) - Mathf.Cos(t) * Mathf.Cos(t) + 2 * Mathf.Cos(t)) + 3f,
+                            10f + Mathf.Sin(time));
+                }
+            }
+
+        }
+
+
+
     }
 }
