1965a795723e36a2a42d066947c62bc7f75fd5e1
carolinecoop <carocoop@terpmail.umd.edu>
2025-02-24 15:14:04 -0500

    Background
    added in a preliminary background in the model testing scene

---
+++ Assets/Scenes/Audio/BackdropCode.cs
@@ -0,0 +1,36 @@
+// UMD IMDM290 
+// Instructor: Myungin Lee
+// Flashing backdrop based on audio amplitude
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backdrop : MonoBehaviour
+{
+    GameObject backdrop;
+    float time = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backdrop = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        backdrop.transform.localScale += new Vector3(50, 30, 1);
+        
+        Renderer backdropRenderer = backdrop.GetComponent<Renderer>();
+        Color backdropColor = Color.HSVToRGB(0f, 0f, 0f);
+        backdropRenderer.material.color = backdropColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        Renderer backdropRenderer = backdrop.GetComponent<Renderer>();
+        Color backdropColor = Color.HSVToRGB(.3f, 1f, 7f * AudioSpectrum.audioAmp);
+        backdropRenderer.material.color = backdropColor;
+
+        Debug.Log("AudioSpectrum.audioAmp: " + AudioSpectrum.audioAmp);
+    }
+}
