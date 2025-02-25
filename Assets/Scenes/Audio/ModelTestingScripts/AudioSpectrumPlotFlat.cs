c48d821984f6332c7c5deaecb865e8197dabb564
schen011 <sophiachen805@gmail.com>
2025-02-24 23:19:18 -0500

    audio plot added

--- Assets/Scenes/Audio/ModelTestingScripts/AudioSpectrumPlotFlat.cs
+++ Assets/Scenes/Audio/ModelTestingScripts/AudioSpectrumPlotFlat.cs
@@ -8,8 +8,17 @@ using UnityEngine;
 public class AudioSpectrumPlotFlat : MonoBehaviour
 {
     // Scale the plot
-    [Range(1f, 100f)]
+    [Range(1f, 200f)]
     public float scale = 10;
+
+    [Range(-300f, 300f)]
+    public float posLR = 0;
+
+    [Range(0f, 1f)]
+    public float huescalar = 0;
+
+    [Range(0f, 1f)]
+    public float hueshift = 0;
 
     // frequency bins are intervals between samples in frequency domain
     GameObject[] sampleBin = new GameObject[AudioSpectrum.FFTSIZE];
@@ -30,13 +39,16 @@ public class AudioSpectrumPlotFlat : MonoBehaviour
         {
             if (sampleBin != null)
             {
-                sampleBin[i].transform.localScale = new Vector3(0.1f, AudioSpectrum.samples[i] * scale * scale, 0.1f);
+                sampleBin[i].transform.localScale = new Vector3(0.5f, AudioSpectrum.samples[i] * scale * scale, 0.5f);
                 //sampleBin[i].GetComponent<Renderer>().material.color = new Color(0.3f + (float)i / 100f, 0.1f + i / 30f, 1+ i / 500f);
-                Color color = Color.HSVToRGB(Mathf.Abs(0.0f + (float)i / 1000f), 0.7f + i / 30f, 1 + i / 500f); // Full saturation and brightness
+                Color color = Color.HSVToRGB(Mathf.Abs((0.0f + (float)i / 1000f)) * huescalar + hueshift, 0.7f + i / 30f, 1 + i / 500f); // Full saturation and brightness
                 sampleBin[i].GetComponent<Renderer>().material.color = color;
 
+                float x = scale * Mathf.Sin((float)i / 100f) + AudioSpectrum.samples[i] * scale ;
+                float y = scale * Mathf.Cos((float)i / 100f) + AudioSpectrum.samples[i] * scale * scale;
 
-                sampleBin[i].transform.position = new Vector3(scale * Mathf.Sin((float)i / 100f) + AudioSpectrum.samples[i] * scale * scale, 0, scale * Mathf.Cos((float)i / 100f) + AudioSpectrum.samples[i] * scale * scale);
+                sampleBin[i].transform.position = new Vector3(x + posLR, y, -50f);
+                
                 // sampleBin[i].transform.Rotate(AudioSpectrum.samples[i], 0f, Mathf.Sin(AudioSpectrum.samples[i]) * AudioSpectrum.samples[i] * scale * scale);
             }
         }
